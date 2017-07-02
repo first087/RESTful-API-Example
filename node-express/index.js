e130cbfbac9a12adfa2711c65f1f7f5ef56c1d5a
Artit Kiuwilai <first087@gmail.com>
2017-07-02 15:51:48 +0700

    Refactoring

--- node-express/index.js
+++ node-express/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!. Node+Express')
+  res.send('Hello World!. Node+Express')
 })
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!')
 })
