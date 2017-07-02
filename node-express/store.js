a3f0d091394d03e5ea461abd70be51adfc074cc8
Artit Kiuwilai <first087@gmail.com>
2017-07-02 16:50:09 +0700

    Implement method getAllItems()

--- node-express/store.js
+++ node-express/store.js
@@ -4,4 +4,5 @@ module.exports = {
   init: () => {
     items = ['Init item']
   },
+  getAllItems: () => items,
 }
--- node-express/test/test_store.js
+++ node-express/test/test_store.js
@@ -3,4 +3,11 @@ const store = require('../store')
 
 describe('Test item store', () => {
   beforeEach(() => store.init())
+
+  it('Get all items', () => {
+    const items = store.getAllItems()
+
+    assert.isArray(items)
+    assert.lengthOf(items, 1)
+  })
 })
