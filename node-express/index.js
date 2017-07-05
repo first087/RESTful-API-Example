6add882e630ec9d55244e53adb2a334c465cc488
Artit Kiuwilai <first087@gmail.com>
2017-07-05 11:21:04 +0700

    Add body parser for parse json from body

--- node-express/index.js
+++ node-express/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const store = require('./store')
 
 const app = express()
+app.use(bodyParser.json())
 
 store.init()
 
