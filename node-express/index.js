532241156e0962699ba3cb1a32229623125fd016
Artit Kiuwilai <first087@gmail.com>
2017-07-05 11:12:45 +0700

    Implement route GET /item/:index for get item by index from store

--- node-express/index.js
+++ node-express/index.js
@@ -10,6 +10,12 @@ app.get('/item/', (req, res) => {
   res.send({ items: store.getAllItems() })
 })
 
+app.get('/item/:index', (req, res) => {
+  const item = store.getItem(req.params.index);
+
+  res.send({ item })
+})
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
