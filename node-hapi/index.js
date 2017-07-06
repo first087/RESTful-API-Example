4ebc8a75bf0cee813b11f936b0a9fb461e067727
Artit Kiuwilai <first087@gmail.com>
2017-07-06 09:11:14 +0700

    Implement route GET /item/{index} for get item by index from store

--- node-hapi/index.js
+++ node-hapi/index.js
@@ -15,6 +15,18 @@ server.route({
   handler: (request, reply) => reply({ items: store.getAllItems() }),
 })
 
+server.route({
+  method: 'GET',
+  path: '/item/{index}',
+  handler: (request, reply) => {
+    const item = store.getItem(Number(request.params.index))
+
+    if (item === undefined) return reply().code(404)
+
+    return reply({ item })
+  },
+})
+
 server.start((err) => {
   if (err) {
     throw err
