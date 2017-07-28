9be9b44b7dcb1bc701964c3f3c64856539a16c06
Artit Kiuwilai <first087@gmail.com>
2017-07-28 22:37:41 +0700

    Add project Java-Spring Boot from Spring Initializr (http://start.spring.io/)

---
+++ java-springboot/src/main/java/com/artitk/RESTfulSpringBoot/ResTfulSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.artitk.RESTfulSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulSpringBootApplication.class, args);
+	}
+}
