1a9e8ac87494d97f8f5bdf944ba6bc9c722be720
Artit Kiuwilai <first087@gmail.com>
2017-07-29 11:04:48 +0700

    Add ctor of ItemController for create Store

--- java-springboot/src/main/java/com/artitk/RESTfulSpringBoot/controller/ItemController.java
+++ java-springboot/src/main/java/com/artitk/RESTfulSpringBoot/controller/ItemController.java
@@ -1,8 +1,13 @@
 package com.artitk.RESTfulSpringBoot.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.artitk.RESTfulSpringBoot.model.Store;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ItemController {
+    private Store store;
 
+    public ItemController() {
+        store = new Store();
+    }
 }
