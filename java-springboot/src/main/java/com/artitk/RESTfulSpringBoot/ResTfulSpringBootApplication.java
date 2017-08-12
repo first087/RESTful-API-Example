506c9c7ccb155eb2dde24d6dd34e65ef26693038
Artit Kiuwilai <first087@gmail.com>
2017-08-12 07:03:16 +0700

    Refactoring

--- java-springboot/src/main/java/com/artitk/RESTfulSpringBoot/ResTfulSpringBootApplication.java
+++ java-springboot/src/main/java/com/artitk/RESTfulSpringBoot/ResTfulSpringBootApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResTfulSpringBootApplication {
+public class RESTfulSpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ResTfulSpringBootApplication.class, args);
+        SpringApplication.run(RESTfulSpringBootApplication.class, args);
     }
 }
