b27f8f8b17849884b816556abcfb226c4cb0f390
Artit Kiuwilai <first087@gmail.com>
2017-10-24 09:39:03 +0700

    Add project Kotlin-Spring Boot from Spring Initializr (http://start.spring.io/)

---
+++ kotlin-springboot/src/main/kotlin/com/artitk/RESTfulSpringBoot/ResTfulSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.artitk.RESTfulSpringBoot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ResTfulSpringBootApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ResTfulSpringBootApplication::class.java, *args)
+}
