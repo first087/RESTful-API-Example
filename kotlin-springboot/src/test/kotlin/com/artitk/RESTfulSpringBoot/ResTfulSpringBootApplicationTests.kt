ce53734fbea1577d3eefab7c4686ddcf6a85dd8e
Artit Kiuwilai <first087@gmail.com>
2017-10-24 10:31:38 +0700

    Formatting

--- kotlin-springboot/src/test/kotlin/com/artitk/RESTfulSpringBoot/ResTfulSpringBootApplicationTests.kt
+++ kotlin-springboot/src/test/kotlin/com/artitk/RESTfulSpringBoot/ResTfulSpringBootApplicationTests.kt
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 class RESTfulSpringBootApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
