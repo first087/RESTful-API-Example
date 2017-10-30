8b30f970c9c7a6808b171c269f894e9f48d650ab
Artit Kiuwilai <first087@gmail.com>
2017-10-30 12:03:16 +0700

    Implement GET /item/{index}

--- kotlin-springboot/src/main/kotlin/com/artitk/RESTfulSpringBoot/controller/ItemController.kt
+++ kotlin-springboot/src/main/kotlin/com/artitk/RESTfulSpringBoot/controller/ItemController.kt
@@ -1,8 +1,11 @@
 package com.artitk.RESTfulSpringBoot.controller
 
+import com.artitk.RESTfulSpringBoot.bean.Item
 import com.artitk.RESTfulSpringBoot.bean.Items
+import com.artitk.RESTfulSpringBoot.exception.NotFoundException
 import com.artitk.RESTfulSpringBoot.model.Store
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,5 +19,17 @@ class ItemController {
     @GetMapping("/item/")
     fun getAllItems(): Items {
         return Items(store!!.getAllItems())
+    }
+
+    @GetMapping("/item/{index}")
+    fun getItem(@PathVariable index: Int): Item {
+        val item: String
+        try {
+            item = store!!.getItem(index)
+        } catch (e: IndexOutOfBoundsException) {
+            throw NotFoundException(e)
+        }
+
+        return Item(item)
     }
 }
