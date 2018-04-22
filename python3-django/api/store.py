69f3be333f5e20a00abb0aabdbbc3043e76102b4
dell <vattanan.bu@gmail.com>
2018-04-22 15:26:13 +0700

    add store.py

---
+++ python3-django/api/store.py
@@ -0,0 +1,25 @@
+item = []
+
+
+def init():
+    pass
+
+
+def getAllItems():
+    pass
+
+
+def get_item(index):
+    pass
+
+
+def add_tem(newItem):
+    pass
+
+
+def update_item(index, updateItem):
+    pass
+
+
+def remove_item(index):
+    pass
