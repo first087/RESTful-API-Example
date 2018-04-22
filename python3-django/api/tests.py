b0a1f3e1274ef7ceef4e906649ea64bf5fc80821
dell <vattanan.bu@gmail.com>
2018-04-22 16:04:22 +0700

    start unit test with tdd

--- python3-django/api/tests.py
+++ python3-django/api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .store import Store
 # Create your tests here.
+
+
+class APITestCase(TestCase):
+    def setUp(self):
+        Store.init()
+
+    def test_get_all_items(self):
+        """
+        Store.get_all_items must return all item in store
+        """
+        item = Store.get_all_items()
+        self.assertIs(type(item), 'list')
+        self.assertIn(len(item), 1)
