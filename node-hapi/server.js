7256cec9f5b459d4d3cd0aae6b9c2bf2a332a01e
Artit Kiuwilai <first087@gmail.com>
2020-07-12 21:33:17 +0700

    Update code style for new version of Hapi

--- node-hapi/server.js
+++ node-hapi/server.js
@@ -1,74 +1,77 @@
-const Hapi = require('hapi')
-const store = require('./store')
+const Hapi = require('@hapi/hapi')
 
-const server = new Hapi.Server()
+const store = require('./store')
 
 store.init()
 
-server.connection({
-  port: 3000,
+const init = async () => {
+  const server = new Hapi.Server({
+    port: 3000,
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/item/',
+    handler: () => ({ items: store.getAllItems() }),
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/item/{index}',
+    handler: (request, h) => {
+      const item = store.getItem(Number(request.params.index))
+
+      if (item === undefined) return h.response().code(404)
+
+      return { item }
+    },
+  })
+
+  server.route({
+    method: 'POST',
+    path: '/item/',
+    handler: (request, h) => {
+      if (typeof request.payload.item !== 'string') return h.response().code(400)
+
+      store.addItem(request.payload.item)
+
+      return h.response().code(201)
+    },
+  })
+
+  server.route({
+    method: 'PUT',
+    path: '/item/{index}',
+    handler: (request, h) => {
+      if (store.getItem(Number(request.params.index)) === undefined) return h.response().code(404)
+
+      if (typeof request.payload.item !== 'string') return h.response().code(400)
+
+      const oldItem = store.updateItem(Number(request.params.index), request.payload.item)
+
+      return { oldItem }
+    },
+  })
+
+  server.route({
+    method: 'DELETE',
+    path: '/item/{index}',
+    handler: (request, h) => {
+      if (store.getItem(Number(request.params.index)) === undefined) return h.response().code(404)
+
+      const removeItem = store.removeItem(Number(request.params.index))
+
+      return { removeItem }
+    },
+  })
+
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+}
+
+process.on('unhandledRejection', (err) => {
+  console.log(err)
+  process.exit(1)
 })
 
-server.route({
-  method: 'GET',
-  path: '/item/',
-  handler: (request, reply) => reply({ items: store.getAllItems() }),
-})
-
-server.route({
-  method: 'GET',
-  path: '/item/{index}',
-  handler: (request, reply) => {
-    const item = store.getItem(Number(request.params.index))
-
-    if (item === undefined) return reply().code(404)
-
-    return reply({ item })
-  },
-})
-
-server.route({
-  method: 'POST',
-  path: '/item/',
-  handler: (request, reply) => {
-    if (typeof request.payload.item !== 'string') return reply().code(400)
-
-    store.addItem(request.payload.item)
-
-    return reply().code(201)
-  },
-})
-
-server.route({
-  method: 'PUT',
-  path: '/item/{index}',
-  handler: (request, reply) => {
-    if (store.getItem(Number(request.params.index)) === undefined) return reply().code(404)
-
-    if (typeof request.payload.item !== 'string') return reply().code(400)
-
-    const oldItem = store.updateItem(Number(request.params.index), request.payload.item)
-
-    return reply({ oldItem })
-  },
-})
-
-server.route({
-  method: 'DELETE',
-  path: '/item/{index}',
-  handler: (request, reply) => {
-    if (store.getItem(Number(request.params.index)) === undefined) return reply().code(404)
-
-    const removeItem = store.removeItem(Number(request.params.index))
-
-    return reply({ removeItem })
-  },
-})
-
-server.start((err) => {
-  if (err) {
-    throw err
-  }
-
-  console.log('Server running at:', server.info.uri);
-})
+init()
