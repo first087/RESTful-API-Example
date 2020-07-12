05b640eb71e5cccd96a83e6369adb28af6383b8e
Artit Kiuwilai <first087@gmail.com>
2020-07-12 20:16:34 +0700

    Refactoring

--- node-express/server.js
+++ node-express/server.js
@@ -4,58 +4,66 @@ const store = require('./store')
 
 store.init()
 
+const getAllItems = (req, res) => {
+  res.send({ items: store.getAllItems() })
+}
+
+const getItem = (req, res) => {
+  const item = store.getItem(Number(req.params.index))
+
+  if (item === undefined) {
+    res.status(404).end()
+    return
+  }
+
+  res.send({ item })
+}
+
+const addItem = (req, res) => {
+  if (typeof req.body.item !== 'string') {
+    res.status(400).end()
+    return
+  }
+
+  store.addItem(req.body.item)
+
+  res.status(201).end()
+}
+
+const updateItem = (req, res) => {
+  if (store.getItem(Number(req.params.index)) === undefined) {
+    res.status(404).end()
+    return
+  }
+
+  if (typeof req.body.item !== 'string') {
+    res.status(400).end()
+    return
+  }
+
+  const oldItem = store.updateItem(Number(req.params.index), req.body.item)
+
+  res.send({ oldItem })
+}
+
+const deleteItem = (req, res) => {
+  if (store.getItem(Number(req.params.index)) === undefined) {
+    res.status(404).end()
+    return
+  }
+
+  const removeItem = store.removeItem(Number(req.params.index))
+
+  res.send({ removeItem })
+}
+
 const app = express()
 
 app
   .use(express.json())
-  .get('/item/', (req, res) => {
-    res.send({ items: store.getAllItems() })
-  })
-  .get('/item/:index', (req, res) => {
-    const item = store.getItem(Number(req.params.index))
-
-    if (item === undefined) {
-      res.status(404).end()
-      return
-    }
-
-    res.send({ item })
-  })
-  .post('/item/', (req, res) => {
-    if (typeof req.body.item !== 'string') {
-      res.status(400).end()
-      return
-    }
-
-    store.addItem(req.body.item)
-
-    res.status(201).end()
-  })
-  .put('/item/:index', (req, res) => {
-    if (store.getItem(Number(req.params.index)) === undefined) {
-      res.status(404).end()
-      return
-    }
-
-    if (typeof req.body.item !== 'string') {
-      res.status(400).end()
-      return
-    }
-
-    const oldItem = store.updateItem(Number(req.params.index), req.body.item)
-
-    res.send({ oldItem })
-  })
-  .delete('/item/:index', (req, res) => {
-    if (store.getItem(Number(req.params.index)) === undefined) {
-      res.status(404).end()
-      return
-    }
-
-    const removeItem = store.removeItem(Number(req.params.index))
-
-    res.send({ removeItem })
-  })
-  .listen(3000, () => {
-    console.log('Example app listening on port 3000!')
-  })
+  .get('/item/', getAllItems)
+  .get('/item/:index', getItem)
+  .post('/item/', addItem)
+  .put('/item/:index', updateItem)
+  .delete('/item/:index', deleteItem)
+  .listen(3000, () => console.log('Example app listening on port 3000!'))
